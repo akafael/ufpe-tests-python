e49a8a96e8566d5d05c6f4259b73247e4dd67d6d
Rafael L <akafael@users.noreply.github.com>
2023-08-22 21:50:01 -0300

    Update test_calculator.py

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -1,7 +1,3 @@
-"""
-Test Calculator.py
-"""
-
 import pytest
 from calculator import Calculator
 
@@ -26,7 +22,6 @@ class TestCalculator():
         assert self.calc.addition(a, b) == expected
 
     def test_addition(self):
-        #self.calc = Calculator()
         assert self.calc.addition(2, 2) == 4
     
     @pytest.mark.skip(reason="Duplicated test case")
@@ -34,19 +29,15 @@ class TestCalculator():
         assert self.calc.addition(10, 10) == 20
     
     def test_subtraction(self):
-        #self.calc = Calculator()
         assert self.calc.subtraction(10, 5) == 5
     
     def test_multiplication(self):
-        #self.calc = Calculator()
         assert self.calc.multiplication(2, 10) == 19
 
     def test_division(self):
-        #self.calc = Calculator()
         assert self.calc.division(10, 2) == 5
     
     @pytest.mark.skipif(skipComplexTests, reason="Skipping complex tests")
     def test_division_by_zero(self):
-        #self.calc = Calculator()
         with pytest.raises(ZeroDivisionError):
             self.calc.division(2,0)
